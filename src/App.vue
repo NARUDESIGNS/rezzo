c6eeed3083edb0afeb37e793b5c54bb081747148
Paul Ibeabuchi <ibeabuchi@worklio.cz>
2023-07-01 23:48:12 +0100

    clean up broiler plate code

---
+++ src/App.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// code here ...
+</script>
+
+<template>
+  <div>
+    <h1>Welcome to Rezzo</h1>
+  </div>
+</template>
+
+<style module lang="scss">
+// styles here...
+</style>
