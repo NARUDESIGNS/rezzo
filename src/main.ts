3fa5059668f979549e8045e889edd0f3f0258435
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-02 23:27:07 +0100

    added pinia

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
+import { createPinia } from "pinia";
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import "./style.css";
 
-createApp(App).use(router).mount("#app");
+const pinia = createPinia();
+const app = createApp(App);
+
+app.use(pinia).use(router).mount("#app");
