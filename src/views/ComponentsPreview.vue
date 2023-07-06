b72159e994d331e361f38094b5e8745019b75d73
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-06 22:11:29 +0100

    modified component preview name

---
+++ src/views/ComponentsPreview.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts">
+import BaseButton from "@/components/base-button/BaseButton.vue";
+</script>
+
+<template>
+  <BaseButton width="350px" disabled> Test Button </BaseButton>
+</template>
+
+<style module lang="scss">
+// styles here...
+</style>
