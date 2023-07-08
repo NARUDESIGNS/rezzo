27a4a797a4ef38f6c8a31b4a97dc67b35d03bff8
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-09 00:58:43 +0100

    added logic and template for base loader

--- src/components/base-loader/BaseLoader.vue
+++ src/components/base-loader/BaseLoader.vue
@@ -1,11 +1,62 @@
 <script setup lang="ts">
-// code here...
+import { computed, useCssModule } from "vue";
+
+const props = withDefaults(
+  defineProps<{
+    size?: number;
+    primary?: boolean;
+    light?: boolean;
+  }>(),
+  {
+    size: 24,
+  }
+);
+
+const $style = useCssModule();
+const size = computed(() => `${props.size}px`);
+const style = computed(() => ({
+  [$style.primary]: props.primary,
+  [$style.light]: props.light,
+}));
 </script>
 
 <template>
-  <!-- component here... -->
+  <span :class="[$style.loader, style]"></span>
 </template>
 
 <style module lang="scss">
-// styles here...
+@use "@/scss/colors";
+
+@mixin border-colors($color: "primary") {
+  border-top-color: colors.use($color);
+  border-right-color: colors.use($color);
+  border-left-color: colors.use($color);
+}
+
+.loader {
+  display: inline-block;
+  width: v-bind(size);
+  height: v-bind(size);
+  border-radius: 50%;
+  border: 3px solid transparent;
+  @include border-colors();
+  animation: loading 1.2s linear infinite;
+
+  @keyframes loading {
+    from {
+      transform: rotate(0deg);
+    }
+    to {
+      transform: rotate(360deg);
+    }
+  }
+}
+
+.primary {
+  @include border-colors();
+}
+
+.light {
+  @include border-colors("loader-light");
+}
 </style>
