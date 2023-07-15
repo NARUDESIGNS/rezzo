4e07cb227cd65e29c91b6fa86186651b93b73a1d
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-16 00:36:37 +0100

    created separete component for label

---
+++ src/components/base-input/FieldLabel.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+defineProps<{
+  required?: boolean;
+  label: string;
+  disabled?: boolean;
+}>();
+</script>
+
+<template>
+  <label :class="[$style.label, { [$style.disabled]: disabled }]">
+    {{ label }}
+    <span v-if="required" :class="$style.required_asterisk">*</span>
+  </label>
+</template>
+
+<style module lang="scss">
+@use "@/scss/colors";
+
+.label {
+  text-transform: capitalize;
+  display: inline-block;
+  margin-bottom: 10px;
+  .required_asterisk {
+    color: colors.use("primary");
+  }
+}
+
+.disabled {
+  opacity: 0.3;
+  cursor: not-allowed;
+}
+</style>
