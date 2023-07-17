b6fcb6f9483ac29b9c09858a4f70b84e284e3936
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-18 00:25:18 +0100

    added Input Number component

---
+++ src/components/base-input/InputNumber.vue
@@ -0,0 +1,41 @@
+<script setup lang="ts">
+import { ref } from "vue";
+import InputText from "./InputText.vue";
+
+const props = withDefaults(
+  defineProps<{
+    max?: number;
+    min?: number;
+  }>(),
+  {
+    max: 10,
+    min: 1,
+  }
+);
+
+const maxValue = ref();
+
+function validateRange() {
+  maxValue.value =
+    maxValue.value > props.max
+      ? props.max
+      : maxValue.value <= 0
+      ? props.min
+      : maxValue;
+  console.log(maxValue.value, props.max);
+}
+</script>
+
+<template>
+  <InputText
+    v-model="maxValue"
+    @change="validateRange()"
+    type="number"
+    :min="min"
+    :max="max"
+  />
+</template>
+
+<style module lang="scss">
+// styles here...
+</style>
