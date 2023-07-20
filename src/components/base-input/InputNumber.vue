866b9972d6b827c433c5322f819c680b9a85aa62
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-20 01:08:58 +0100

    added min and max props to input number component

--- src/components/base-input/InputNumber.vue
+++ src/components/base-input/InputNumber.vue
@@ -1,9 +1,13 @@
 <script setup lang="ts">
 import InputText from "./InputText.vue";
+defineProps<{
+  min?: number;
+  max?: number;
+}>();
 </script>
 
 <template>
-  <InputText type="number" />
+  <InputText type="number" :min="min" :max="max" />
 </template>
 
 <style module lang="scss">
