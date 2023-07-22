298f3eb2b5d030507557cfea94c356eb0bde0f7a
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-22 16:36:28 +0100

    updated password component and added show/hide password functionalities

--- src/components/base-input/InputPassword.vue
+++ src/components/base-input/InputPassword.vue
@@ -1,11 +1,63 @@
 <script setup lang="ts">
+import EyeClose from "@/assets/EyeClose.vue";
+import EyeOpen from "@/assets/EyeOpen.vue";
+import { ref } from "vue";
 import InputText from "./InputText.vue";
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+defineProps<{
+  modelValue?: string;
+}>();
+
+defineEmits<{
+  (e: "update:modelValue", modelValue: string): void;
+}>();
+
+const type = ref("password");
+const showPassword = () => (type.value = "text");
+const hidePassword = () => (type.value = "password");
 </script>
 
 <template>
-  <InputText type="password" />
+  <div :class="[$style.container, $attrs.class]">
+    <InputText
+      :type="type"
+      :value="modelValue"
+      @input="
+        $emit(
+          'update:modelValue',
+          ($event.target as HTMLInputElement).value.trim()
+        )
+      "
+      v-bind="$attrs"
+    />
+    <EyeClose
+      v-if="type === 'password'"
+      @click="showPassword"
+      :class="$style.icon"
+    />
+    <EyeOpen
+      v-if="type === 'text'"
+      @click="hidePassword"
+      :class="$style.icon"
+    />
+  </div>
 </template>
 
 <style module lang="scss">
-// styles here...
+.container {
+  width: 100%;
+  position: relative;
+
+  .icon {
+    position: absolute;
+    right: 70px;
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+  }
+}
 </style>
