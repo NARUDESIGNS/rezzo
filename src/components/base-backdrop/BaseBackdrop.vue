eb87e065f5e46c287c801888fe5a616d1bd52c14
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-26 01:05:39 +0100

    added Base Backdrop component

--- src/components/base-backdrop/BaseBackdrop.vue
+++ src/components/base-backdrop/BaseBackdrop.vue
@@ -1,11 +1,21 @@
 <script setup lang="ts">
-// code here...
+defineProps<{
+  visible?: boolean;
+}>();
 </script>
 
 <template>
-  <!-- component here... -->
+  <div v-if="visible" :class="$style.backdrop"></div>
 </template>
 
 <style module lang="scss">
-// styles here...
+.backdrop {
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  left: 0;
+  background: black;
+  opacity: 0.6;
+}
 </style>
