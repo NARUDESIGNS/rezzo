725aa53246b71e2d2caa3346c6eb147887ef4337
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-07-30 23:11:25 +0100

    WIP: added date component, using Vue Date Picker library

--- src/components/base-input/InputDate.vue
+++ src/components/base-input/InputDate.vue
@@ -1,9 +1,31 @@
 <script setup lang="ts">
-// code here...
+import DatePicker from "@vuepic/vue-datepicker";
+import { ref } from "vue";
+import InputText from "./InputText.vue";
+
+const date = ref(new Date());
 </script>
 
 <template>
-  <!-- component here... -->
+  <DatePicker v-model="date" :enable-time-picker="false" format="dd/MM/yyyy">
+    <template #dp-input="{ value, onInput, onEnter, onTab, onBlur }">
+      <div :class="$style.container">
+        <InputText
+          autocomplete="off"
+          :value="value"
+          :disabled="false"
+          placeholder="DD/MM/YY"
+          :class="$style.input"
+          :required="true"
+          @input="onInput"
+          @keydown.enter="onEnter"
+          @keydown.tab="onTab"
+          @blur="onBlur"
+          @keyup.esc="DatePicker?.closeMenu()"
+        />
+      </div>
+    </template>
+  </DatePicker>
 </template>
 
 <style module lang="scss">
