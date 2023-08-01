e0240489985fc3a5a7c394b00596e8dee1661751
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-01 08:14:39 +0100

    added base search component

--- src/components/base-search/BaseSearch.vue
+++ src/components/base-search/BaseSearch.vue
@@ -1,11 +1,52 @@
 <script setup lang="ts">
-// code here...
+import SearchIcon from "@/assets/SearchIcon.vue";
+import InputText from "@/components/base-input/InputText.vue";
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+defineProps<{
+  modelValue?: string;
+}>();
+
+defineEmits<{
+  (e: "update:modelValue", modelValue: string): void;
+}>();
 </script>
 
 <template>
-  <!-- component here... -->
+  <div :class="[$style.container, $attrs.class]">
+    <InputText
+      :value="modelValue"
+      @input="
+        $emit(
+          'update:modelValue',
+          ($event.target as HTMLInputElement).value.trim()
+        )
+      "
+      v-bind="$attrs"
+      :class="$style.input"
+    />
+    <SearchIcon :class="$style.icon" />
+  </div>
 </template>
 
 <style module lang="scss">
-// styles here...
+.container {
+  width: 100%;
+  position: relative;
+
+  .icon {
+    position: absolute;
+    right: 17px;
+    top: 50%;
+    transform: translateY(-50%);
+    cursor: pointer;
+  }
+
+  .input {
+    padding-right: 50px;
+  }
+}
 </style>
