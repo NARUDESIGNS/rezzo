a78458deb08481fff409714806ebcf3dc28f234d
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-02 23:44:27 +0100

    updated NotFound component with added illustration :)

--- src/views/NotFoundView.vue
+++ src/views/NotFoundView.vue
@@ -1,20 +1,24 @@
 <script setup lang="ts">
-// code here...
+import NotFoundIllustration from "@/assets/illustrations/NotFoundIllustration.vue";
 </script>
 
 <template>
   <div :class="$style.not_found">
+    <NotFoundIllustration :class="$style.illustration" />
     <h1 :class="$style.not_found_text">
       404 <br />
       Not Found!
     </h1>
+    <!-- // TODO: replace with <BaseAction /> -->
+    <RouterLink :to="{ name: 'home' }" :class="$style.link">Go Home</RouterLink>
   </div>
 </template>
 
 <style module lang="scss">
 .not_found {
-  height: 100vh;
+  height: 80vh;
   display: flex;
+  flex-direction: column;
   align-items: center;
   justify-content: center;
 
@@ -22,6 +26,15 @@
     font-size: 3rem;
     font-weight: 800;
     text-align: center;
+    margin-top: 10px;
+  }
+
+  .illustration {
+    transform: translateX(-20px);
+  }
+
+  .link:hover {
+    text-decoration: underline;
   }
 }
 </style>
