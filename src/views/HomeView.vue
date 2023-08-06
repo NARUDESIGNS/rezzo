ef3d6efbf4a7658a1914541cebbee113805917b1
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-06 19:31:38 +0100

    fixed minor issue with primary logo and added it to Home View

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-// logic here...
+import BlueLogo from "@/assets/BlueLogo.vue";
 </script>
 
 <template>
   <div>
+    <BlueLogo />
     <h1>Welcome to Rezzo, where we land you your dream role!</h1>
   </div>
 </template>
