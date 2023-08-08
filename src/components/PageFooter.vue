e429816a01a3129faf0c920610dbb69e9934da63
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-08 22:22:44 +0100

    added footer

---
+++ src/components/PageFooter.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import BaseAction from "./base-action/BaseAction.vue";
+
+defineProps<{
+  darkMode?: boolean;
+}>();
+</script>
+
+<template>
+  <div :class="[$style.footer, { [$style.darkMode]: darkMode }]">
+    <p :class="$style.content">MADE WITH 🖤 BY</p>
+    <BaseAction href="https://github.com/narudesigns" :dark-mode="darkMode">
+      @narudesigns
+    </BaseAction>
+    <p>© {{ new Date().getUTCFullYear() }}</p>
+  </div>
+</template>
+
+<style module lang="scss">
+.content {
+  width: fit-content;
+}
+.darkMode {
+  color: white !important;
+}
+.footer {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  gap: 5px;
+}
+</style>
