cac047a98dc7aeb08f9bb104f0f8c95b1248b360
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-11 23:50:16 +0100

    added multi-text input file logic and functionalities

--- src/components/base-input/InputMultiText.vue
+++ src/components/base-input/InputMultiText.vue
@@ -1,11 +1,163 @@
 <script setup lang="ts">
-// code here...
+import XIcon from "@/assets/XIcon.vue";
+import ButtonLabel from "@/components/base-button/ButtonLabel.vue";
+import { capitalize, computed, ref, useCssModule } from "vue";
+
+defineOptions({
+  inheritAttrs: false,
+});
+
+const props = defineProps<{
+  required?: boolean;
+  error?: boolean;
+  errorMsg?: string;
+  disabled?: boolean;
+  placeholder?: string;
+  showButton?: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "items-added", data: string[]): void;
+  (e: "input-removed", inputLabel: string): void;
+}>();
+
+const $style = useCssModule();
+const styles = computed(() => ({
+  [$style.disabled]: props.disabled,
+  [$style.error]: props.error,
+}));
+
+const items = ref<string[]>(["Vue", "React", "Nodejs"]);
+const removeItem = (index: number) => items.value.splice(index, 1);
+
+const emitData = () => emit("items-added", items.value);
 </script>
 
 <template>
-  <!-- component here... -->
+  <div :class="$style.container">
+    <template v-if="items.length">
+      <div :class="$style.item_wrap">
+        <div v-for="(item, index) in items" :key="index" :class="$style.item">
+          <span>{{ item }}</span>
+          <XIcon :class="$style.remove_icon" @click="removeItem(index)" />
+        </div>
+      </div>
+    </template>
+    <input
+      @change="emitData"
+      @keydown.tab.prevent="(e) => items.push(capitalize((e.target as HTMLInputElement).value))"
+      :class="[$style.input, styles, $attrs.class]"
+      :disabled="disabled"
+      :placeholder="placeholder"
+      v-bind="$attrs"
+    />
+    <template v-if="showButton">
+      <ButtonLabel
+        v-if="!required && !disabled"
+        minus
+        :class="$style.minus_btn"
+        @click="$emit('input-removed')"
+      />
+      <ButtonLabel v-else :class="$style.hide_minus_btn" />
+    </template>
+  </div>
+  <p v-if="error" :class="$style.error_msg">
+    {{ errorMsg || "This is a required field!" }}
+  </p>
 </template>
 
 <style module lang="scss">
-// styles here...
+@use "@/scss/colors";
+
+::placeholder {
+  font-style: italic;
+}
+
+.container {
+  min-width: fit-content;
+  width: 100%;
+
+  &:hover .minus_btn {
+    visibility: visible;
+    animation: fade 0.2s linear forwards;
+
+    @keyframes fade {
+      from {
+        opacity: 0;
+      }
+      to {
+        opacity: 1;
+      }
+    }
+  }
+
+  .minus_btn {
+    margin-left: 10px;
+    visibility: hidden;
+    opacity: 0;
+  }
+
+  .hide_minus_btn {
+    margin-left: 10px;
+    visibility: hidden;
+    pointer-events: none;
+  }
+}
+
+.item_wrap {
+  display: flex;
+  align-items: center;
+  flex-wrap: wrap;
+  gap: 10px;
+  margin-bottom: 20px;
+
+  .item {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    gap: 10px;
+    color: colors.use("text-light");
+    background-color: colors.use("primary");
+    border-radius: 100px;
+    height: 35px;
+    padding: 0 15px;
+    width: fit-content;
+
+    .remove_icon {
+      cursor: pointer;
+    }
+  }
+}
+.input {
+  padding: 15px 20px;
+  border-radius: 8px;
+  border: 1px solid colors.use("border-light");
+  min-width: 250px;
+  width: 100%;
+  font-size: 1.1rem;
+
+  &:focus {
+    outline: none;
+  }
+}
+
+.disabled {
+  opacity: 0.3;
+  cursor: not-allowed;
+}
+
+.error {
+  border: 1px solid colors.use("danger");
+}
+
+.error_msg {
+  color: colors.use("danger");
+  margin-top: 5px;
+}
+
+@media screen and (max-width: 400px) {
+  .input {
+    padding: 15px;
+  }
+}
 </style>
