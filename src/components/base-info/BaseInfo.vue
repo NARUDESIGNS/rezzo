2efa806060c397cf892488da17254bc2b57010a3
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-13 00:38:12 +0100

    added base info component and used it in preview

--- src/components/base-info/BaseInfo.vue
+++ src/components/base-info/BaseInfo.vue
@@ -1,11 +1,119 @@
 <script setup lang="ts">
-// code here...
+import InfoIcon from "@/assets/InfoIcon.vue";
+import XIcon from "@/assets/XIcon.vue";
+import { computed, ref, useCssModule } from "vue";
+
+const props = defineProps<{
+  enableClose?: boolean;
+  primary?: boolean;
+  danger?: boolean;
+  warning?: boolean;
+}>();
+
+defineEmits<{
+  (e: "close"): void;
+}>();
+
+const $style = useCssModule();
+const styles = computed(() => ({
+  [$style.primary]: props.primary,
+  [$style.danger]: props.danger,
+  [$style.warning]: props.warning,
+}));
+
+const visible = ref(false);
 </script>
 
 <template>
-  <!-- component here... -->
+  <Transition name="fade">
+    <div v-if="!visible" :class="[$style.wrap, styles]">
+      <XIcon
+        v-if="enableClose"
+        :class="$style.close_icon"
+        @click="$emit('close'), (visible = true)"
+      />
+      <InfoIcon :class="$style.info_icon" />
+      <div :class="$style.content">
+        <header :class="$style.header">
+          <slot name="header" />
+        </header>
+        <slot />
+      </div>
+    </div>
+  </Transition>
 </template>
 
 <style module lang="scss">
-// styles here...
+@use "@/scss/colors";
+
+.wrap {
+  position: relative;
+  padding: 20px;
+  padding-right: 40px;
+  border: 1px solid colors.use("border-dark");
+  border-radius: 7px;
+  display: flex;
+  align-items: flex-start;
+  gap: 20px;
+  margin-bottom: 20px;
+
+  .header {
+    margin-bottom: 10px;
+  }
+
+  .info_icon {
+    width: 35px;
+  }
+
+  .close_icon {
+    position: absolute;
+    top: 10px;
+    right: 10px;
+    cursor: pointer;
+
+    &:hover {
+      color: darken($color: colors.use("border-dark"), $amount: 5);
+      transition: 0.2s ease;
+    }
+  }
+
+  &:global(.fade-leave-active) {
+    transition: 0.3s ease;
+    transform: translateY(0);
+  }
+  &:global(.fade-leave-to) {
+    opacity: 0;
+    transform: translateY(-10px);
+  }
+}
+
+.primary {
+  border-color: colors.use("primary");
+  background-color: lighten($color: colors.use("primary"), $amount: 40);
+
+  .info_icon,
+  .header {
+    color: colors.use("primary");
+  }
+}
+
+.warning {
+  border-color: colors.use("warning");
+  background-color: lighten($color: colors.use("warning"), $amount: 30);
+
+  .info_icon,
+  .header {
+    color: darken($color: colors.use("warning"), $amount: 20);
+  }
+}
+
+.danger {
+  border-color: colors.use("danger");
+  background-color: lighten($color: colors.use("danger"), $amount: 30);
+
+  .info_icon,
+  .header {
+    color: colors.use("danger");
+  }
+}
 </style>
--- src/views/ComponentsPreview.vue
+++ src/views/ComponentsPreview.vue
@@ -4,6 +4,7 @@ import BaseAction from "@/components/base-action/BaseAction.vue";
 import BaseBackdrop from "@/components/base-backdrop/BaseBackdrop.vue";
 import BaseButton from "@/components/base-button/BaseButton.vue";
 import ButtonLabel from "@/components/base-button/ButtonLabel.vue";
+import BaseInfo from "@/components/base-info/BaseInfo.vue";
 import FieldLabel from "@/components/base-input/FieldLabel.vue";
 import InputCheckbox from "@/components/base-input/InputCheckbox.vue";
 import InputDate from "@/components/base-input/InputDate.vue";
@@ -14,7 +15,7 @@ import InputText from "@/components/base-input/InputText.vue";
 import InputTextArea from "@/components/base-input/InputTextArea.vue";
 import BaseLoader from "@/components/base-loader/BaseLoader.vue";
 import BaseSearch from "@/components/base-search/BaseSearch.vue";
-import { ref } from "vue";
+import { computed, ref } from "vue";
 
 const checkbox = ref(true);
 const checkboxFilled = ref(true);
@@ -29,133 +30,174 @@ const textarea = ref("Testing notes...");
 const date = ref(new Date());
 const searchQuery = ref();
 
-const showBackdrop = ref(false);
+const infoType = ref();
+const infoProps = computed(() => ({
+  primary: infoType.value === "primary",
+  warning: infoType.value === "warning",
+  danger: infoType.value === "danger",
+}));
+
+const showBackdrop = ref();
 
 const logDetails = (info?: string) =>
   alert(info ?? "input filed will be removed when this button is clicked");
 </script>
 
 <template>
-  <h2 :class="$style.header">Buttons</h2>
-  <div :class="$style.content">
-    <BaseButton> Primary Button </BaseButton>
-    <BaseButton is-loading />
+  <div :class="$style.wrap">
+    <BaseInfo enable-close v-bind="infoProps">
+      <template #header>
+        <h4>Quick Note</h4>
+      </template>
+      <p>
+        By the way, make sure to go through each component and explore as much
+        as you want. These components are reusable blocks which will be used in
+        building the
+        <BaseAction
+          href="https://github.com/narudesigns/rezzo"
+          v-bind="infoProps"
+        >
+          rezzo
+        </BaseAction>
+        project from start to finish!
+      </p>
+      <div :class="$style.info_options_wrap">
+        <span :class="$style.info_option" @click="infoType = 'primary'">
+          primary
+        </span>
+        <span :class="$style.info_option" @click="infoType = 'warning'">
+          warning</span
+        >
+        <span :class="$style.info_option" @click="infoType = 'danger'">
+          danger
+        </span>
+        <span :class="$style.info_option" @click="infoType = null">reset</span>
+      </div>
+    </BaseInfo>
+    <h2 :class="$style.header">Buttons</h2>
+    <div :class="$style.content">
+      <BaseButton> Primary Button </BaseButton>
+      <BaseButton is-loading />
 
-    <BaseButton danger> Danger Button </BaseButton>
-    <BaseButton is-loading danger />
+      <BaseButton danger> Danger Button </BaseButton>
+      <BaseButton is-loading danger />
 
-    <BaseButton normal> Normal Button </BaseButton>
-    <BaseButton is-loading normal />
+      <BaseButton outline> Normal Button </BaseButton>
+      <BaseButton is-loading outline />
 
-    <BaseButton danger disabled> Danger Button </BaseButton>
-    <BaseButton disabled> Primary Button </BaseButton>
-    <BaseButton normal disabled> Normal Button </BaseButton>
+      <BaseButton danger disabled> Danger Button </BaseButton>
+      <BaseButton disabled> Primary Button </BaseButton>
+      <BaseButton outline disabled> Normal Button </BaseButton>
 
-    <ButtonLabel plus>Add Button Label</ButtonLabel>
-    <ButtonLabel minus>Minus Button Label</ButtonLabel>
-    <ButtonLabel plus disabled>Add Button Label</ButtonLabel>
-    <ButtonLabel minus disabled>Minus Button Label</ButtonLabel>
-    <ButtonLabel plus :size="40" />
-    <ButtonLabel minus :size="40" />
+      <ButtonLabel plus>Add Button Label</ButtonLabel>
+      <ButtonLabel minus>Minus Button Label</ButtonLabel>
+      <ButtonLabel plus disabled>Add Button Label</ButtonLabel>
+      <ButtonLabel minus disabled>Minus Button Label</ButtonLabel>
+      <ButtonLabel plus :size="40" />
+      <ButtonLabel minus :size="40" />
 
-    <BaseAction
-      href="/"
-      :action="() => logDetails('Test Action')"
-      label="Go Home"
-    />
+      <BaseAction
+        href="/"
+        :action="() => logDetails('Test Action')"
+        label="Go Home"
+      />
 
-    <BaseAction
-      :action="() => logDetails('Test Danger Action')"
-      label="Danger Action"
-      danger
-    />
+      <BaseAction
+        :action="() => logDetails('Test Danger Action')"
+        label="Danger Action"
+        danger
+      />
 
-    <BaseAction :action="() => {}" label="Disabled Action" disabled />
+      <BaseAction :action="() => {}" label="Disabled Action" disabled />
+    </div>
+
+    <h2 :class="$style.header">Loaders</h2>
+    <div :class="$style.content">
+      <BaseLoader :size="50" />
+      <BaseLoader :size="40" />
+      <BaseLoader :size="30" />
+      <BaseLoader />
+    </div>
+
+    <h2 :class="$style.header">Checkboxes</h2>
+    <div :class="$style.content">
+      <InputCheckbox v-model="checkbox" />
+      <InputCheckbox v-model="checkboxFilled" filled />
+      <InputCheckbox :size="20" />
+      <InputCheckbox :size="20" filled />
+      <InputCheckbox checked disabled />
+      <InputCheckbox id="checkBox" filled />
+      <label for="checkBox">Remember Me</label>
+    </div>
+
+    <h2 :class="$style.header">Input Fields</h2>
+    <div :class="[$style.content, $style.input_fields]">
+      <BaseSearch v-model="searchQuery" placeholder="search..." />
+
+      <FieldLabel label="First Name" required />
+      <InputText v-model="firstName" required />
+
+      <FieldLabel label="lastName" />
+      <InputText v-model="lastName" @input-removed="() => logDetails()" />
+
+      <FieldLabel label="email" required />
+      <InputText
+        v-model="email"
+        error-msg="Invalid Email Address!"
+        required
+        error
+      />
+
+      <FieldLabel label="Middle Name" disabled />
+      <InputText v-model="middleName" type="text" disabled />
+
+      <FieldLabel label="Placeholder" />
+      <InputText
+        type="text"
+        placeholder="Tell me a secret here..."
+        @input-removed="() => logDetails()"
+      />
+
+      <FieldLabel label="Password" required />
+      <InputPassword
+        v-model="password"
+        @input-removed="() => logDetails()"
+        required
+      />
+
+      <FieldLabel label="Number" />
+      <InputNumber
+        v-model="numbr"
+        @input-removed="() => logDetails()"
+        :min="1"
+        :max="10"
+      />
+
+      <FieldLabel label="Text Area" required />
+      <InputTextArea
+        v-model="textarea"
+        required
+        @input-removed="() => logDetails()"
+      />
+
+      <FieldLabel label="Date" required />
+      <InputDate v-model="date" />
+
+      <FieldLabel label="Skills" required />
+      <InputMultiText placeholder="add items clicking on the tab key" />
+    </div>
+
+    <h2 :class="$style.header">Actions</h2>
+    <div :class="$style.content">
+      <BaseButton @click="showBackdrop = true"> Show Backdrop</BaseButton>
+      <BaseBackdrop
+        :visible="showBackdrop"
+        @click.self="showBackdrop = false"
+      />
+    </div>
+
+    <PageFooter />
   </div>
-
-  <h2 :class="$style.header">Loaders</h2>
-  <div :class="$style.content">
-    <BaseLoader :size="50" />
-    <BaseLoader :size="40" />
-    <BaseLoader :size="30" />
-    <BaseLoader />
-  </div>
-
-  <h2 :class="$style.header">Checkboxes</h2>
-  <div :class="$style.content">
-    <InputCheckbox v-model="checkbox" />
-    <InputCheckbox v-model="checkboxFilled" filled />
-    <InputCheckbox :size="20" />
-    <InputCheckbox :size="20" filled />
-    <InputCheckbox checked disabled />
-    <InputCheckbox id="checkBox" filled />
-    <label for="checkBox">Remember Me</label>
-  </div>
-
-  <h2 :class="$style.header">Input Fields</h2>
-  <div :class="[$style.content, $style.input_fields]">
-    <BaseSearch v-model="searchQuery" placeholder="search..." />
-
-    <FieldLabel label="First Name" required />
-    <InputText v-model="firstName" required />
-
-    <FieldLabel label="lastName" />
-    <InputText v-model="lastName" @input-removed="() => logDetails()" />
-
-    <FieldLabel label="email" required />
-    <InputText
-      v-model="email"
-      error-msg="Invalid Email Address!"
-      required
-      error
-    />
-
-    <FieldLabel label="Middle Name" disabled />
-    <InputText v-model="middleName" type="text" disabled />
-
-    <FieldLabel label="Placeholder" />
-    <InputText
-      type="text"
-      placeholder="Tell me a secret here..."
-      @input-removed="() => logDetails()"
-    />
-
-    <FieldLabel label="Password" required />
-    <InputPassword
-      v-model="password"
-      @input-removed="() => logDetails()"
-      required
-    />
-
-    <FieldLabel label="Number" />
-    <InputNumber
-      v-model="numbr"
-      @input-removed="() => logDetails()"
-      :min="1"
-      :max="10"
-    />
-
-    <FieldLabel label="Text Area" required />
-    <InputTextArea
-      v-model="textarea"
-      required
-      @input-removed="() => logDetails()"
-    />
-
-    <FieldLabel label="Date" required />
-    <InputDate v-model="date" />
-
-    <FieldLabel label="Skills" required />
-    <InputMultiText placeholder="add items clicking on the tab key" />
-  </div>
-
-  <h2 :class="$style.header">Actions</h2>
-  <div :class="$style.content">
-    <BaseButton @click="showBackdrop = true"> Show Backdrop</BaseButton>
-    <BaseBackdrop :visible="showBackdrop" @click.self="showBackdrop = false" />
-  </div>
-
-  <PageFooter />
 </template>
 
 <style module lang="scss">
@@ -163,6 +205,25 @@ const logDetails = (info?: string) =>
 .test {
   width: 500px;
 }
+
+.wrap {
+  padding: 20px;
+}
+
+.info_options_wrap {
+  display: flex;
+  gap: 10px;
+  margin-top: 10px;
+
+  .info_option {
+    padding: 3px 10px;
+    font-size: 14px;
+    border: 1px solid colors.use(v-bind(infoType));
+    border-radius: 100px;
+    cursor: pointer;
+  }
+}
+
 .header {
   margin-bottom: 20px;
 }
