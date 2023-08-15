7d8306cee73bfd1c869fb7e7e19d8b05358925d2
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-15 22:49:30 +0100

    added PageCenter component to center page and provide navbar & footer to child component

---
+++ src/components/PageCenter.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+import PageFooter from "@/components/PageFooter.vue";
+import PageNav from "@/components/PageNav.vue";
+</script>
+
+<template>
+  <div :class="$style.overall_container">
+    <PageNav />
+    <slot />
+    <PageFooter />
+  </div>
+</template>
+
+<style module lang="scss">
+@use "@/scss/colors";
+
+.overall_container {
+  color: colors.use("text");
+  min-height: 95vh;
+  display: flex;
+  flex-direction: column;
+  padding: 20px;
+}
+</style>
