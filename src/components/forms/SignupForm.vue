7c150c6a8b75028906ddf73a6ced335e64291a25
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-29 00:10:45 +0100

    added signup form

--- src/components/forms/SignupForm.vue
+++ src/components/forms/SignupForm.vue
@@ -1,11 +1,59 @@
 <script setup lang="ts">
-// code here...
+import FieldLabel from "@/components/base-input/FieldLabel.vue";
+import InputPassword from "@/components/base-input/InputPassword.vue";
+import InputText from "@/components/base-input/InputText.vue";
+import { reactive } from "vue";
+import BaseAction from "../base-action/BaseAction.vue";
+import BaseButton from "../base-button/BaseButton.vue";
+import FormHeader from "../base-input/FormHeader.vue";
+
+interface IForm {
+  firstName: string;
+  lastName: string;
+  email: string;
+  password: string;
+  stayLoggedIn: boolean;
+}
+
+const form: IForm = reactive({
+  firstName: "",
+  lastName: "",
+  email: "",
+  password: "",
+  stayLoggedIn: false,
+});
 </script>
 
 <template>
-  <!-- component here... -->
+  <form :class="$style.wrap">
+    <FormHeader>Create Account</FormHeader>
+    <FieldLabel label="First Name" required />
+    <InputText v-model="form.firstName" required />
+    <FieldLabel label="Last Name" required />
+    <InputText v-model="form.lastName" required />
+    <FieldLabel label="Email" required />
+    <InputText v-model="form.email" required />
+    <FieldLabel label="Password" required />
+    <InputPassword v-model="form.password" required />
+    <FieldLabel label="Confirm Password" required />
+    <InputPassword required />
+    <BaseButton :class="$style.btn">Login</BaseButton>
+    <p>
+      Already have an account?
+      <BaseAction>Login</BaseAction>
+    </p>
+  </form>
 </template>
 
 <style module lang="scss">
-// styles here...
+@use "@/scss/colors";
+
+.wrap {
+  width: 100%;
+
+  .btn {
+    width: 100%;
+    margin: 25px 0;
+  }
+}
 </style>
