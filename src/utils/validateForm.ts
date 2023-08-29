e7f6ef2f802e319d62e84d4709396b899745b196
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-08-29 01:05:28 +0100

    added function to validate form

---
+++ src/utils/validateForm.ts
@@ -0,0 +1,19 @@
+/**
+ * Validates form
+ * @param form - form object
+ * @param requiredFields - array of required fields
+ * @param hasErrorField - boolean
+ * @returns returns a boolean
+ */
+
+export function validateForm<T extends object>(
+  form: T,
+  requiredFields: string[],
+  hasErrorField: boolean
+) {
+  if (hasErrorField) return false;
+  for (const key of requiredFields) {
+    if (key in form && (key === "" || key === undefined)) return false;
+  }
+  return true;
+}
