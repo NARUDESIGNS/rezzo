5f4331985ad4ce51df4ae587cf144445c16847cc
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-09-09 23:40:12 +0100

    added util method to convert date-string to actual date

---
+++ src/utils/stringToDate.ts
@@ -0,0 +1,16 @@
+/**
+ * Converts string to date
+ * @param dateString - string
+ * @returns date value
+ * @example
+ * // Example usage:
+  const dateString = '05/03/2023'; // DD/MM/YYYY format
+  const date = stringToDate(dateString);
+ */
+
+export function stringToDate(dateString: string) {
+  console.log(dateString);
+  const [day, month, year] = dateString.split("/");
+  // Note: Months are 0-based in JavaScript Date objects, so we subtract 1 from the month
+  return new Date(Number(year), Number(month) - 1, Number(day));
+}
