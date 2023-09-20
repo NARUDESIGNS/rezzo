6ce1d52ceaa518582919f4ef7e369a014a316280
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-09-20 23:55:30 +0100

    added switch component

--- src/components/base-input/InputSwitch.vue
+++ src/components/base-input/InputSwitch.vue
@@ -1,11 +1,70 @@
 <script setup lang="ts">
-// code here...
+defineProps<{
+  modelValue: boolean;
+  id?: string;
+  disabled?: boolean;
+}>();
 </script>
 
 <template>
-  <!-- component here... -->
+  <input
+    :class="$style.input"
+    type="checkbox"
+    :id="id"
+    ref="input"
+    :checked="modelValue"
+    @change="
+      $emit(
+        'update:modelValue',
+        ($event.target as unknown as HTMLInputElement).checked
+      )
+    "
+    :disabled="disabled"
+  />
 </template>
 
 <style module lang="scss">
-// styles here...
+@use "@/scss/colors";
+
+.input {
+  vertical-align: middle;
+  width: 40px;
+  height: 20px;
+  appearance: none;
+  -webkit-appearance: none;
+  background-color: #e4e4e4;
+  cursor: pointer;
+  border-radius: 20px;
+  outline: none;
+  transition: ease 0.3s;
+  position: relative;
+  -webkit-tap-highlight-color: transparent;
+}
+.input:before {
+  content: "";
+  display: block;
+  height: 15px;
+  width: 15px;
+  background-color: hsl(225, 100%, 98%);
+  border-radius: 50%;
+  position: absolute;
+  left: 3px;
+  top: 50%;
+  transform: translateY(-50%);
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2);
+  transition: ease 0.3s;
+}
+
+.input:checked {
+  background: colors.use("primary");
+}
+
+.input:checked::before {
+  left: 22px;
+}
+
+.input:disabled {
+  opacity: 0.3;
+  cursor: not-allowed;
+}
 </style>
