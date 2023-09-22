ea1fe0e80bd2f2a336fe7014f5082d32f298e078
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-09-22 23:42:33 +0100

    modified create-resume page according to new structure to separate resume sections

--- src/views/CreateResume.vue
+++ src/views/CreateResume.vue
@@ -7,20 +7,12 @@ import InputDate from "@/components/base-input/InputDate.vue";
 import InputMultiText from "@/components/base-input/InputMultiText.vue";
 import InputText from "@/components/base-input/InputText.vue";
 import InputTextArea from "@/components/base-input/InputTextArea.vue";
+import PersonalInfoSection from "@/components/forms/resume/PersonalInfoSection.vue";
 import type { EducationType } from "@/types/EducationType";
 import type { ExperienceType } from "@/types/ExperienceType";
-import type { PersonalInfoType } from "@/types/PersonalInfoType";
 import { ref } from "vue";
 
 type Skills = string[];
-
-const personalInfo = ref<PersonalInfoType>({
-  fullName: "Paul Ibeabuchi",
-  email: "ibeabuchi@gmail.com",
-  phone: 2348160564736,
-  location: "Lagos Nigeria",
-  linkedIn: "https://www.linkedin.com/in/narudesigns",
-});
 
 const skills = ref<Skills>(["Vue", "React", "TypeScript", "Nodejs"]);
 
@@ -54,28 +46,7 @@ const education = ref<EducationType>({
       </p>
       <main :class="$style.details">
         <div :class="$style.section">
-          <h3 :class="$style.sub_header">Personal Information</h3>
-          <p :class="$style.description">
-            Add your personal details which will be used. This will be at the
-            top of your resume and usually includes details with which potential
-            recruiters can reach you.
-          </p>
-          <div :class="$style.personal_info_form">
-            <FieldLabel label="Full Name" required />
-            <InputText v-model="personalInfo.fullName" required />
-            <FieldLabel label="Email" required />
-            <InputText v-model="personalInfo.email" required />
-            <FieldLabel label="Phone" required />
-            <InputText v-model="personalInfo.phone" type="number" required />
-            <FieldLabel label="Location" required />
-            <InputText v-model="personalInfo.location" required />
-            <FieldLabel label="LinkedIn" required />
-            <InputText v-model="personalInfo.linkedIn" required />
-            <div :class="$style.btn_label_wrap">
-              <ButtonLabel label="Add New Field" plus />
-              <ButtonLabel label="Remove Fields" minus />
-            </div>
-          </div>
+          <PersonalInfoSection />
 
           <div :class="$style.section">
             <h3 :class="$style.sub_header">Skills</h3>
@@ -203,14 +174,6 @@ const education = ref<EducationType>({
     .sub_header {
       color: colors.use("primary");
       margin: 50px 0 10px 0;
-    }
-
-    .btn_label_wrap {
-      display: flex;
-      align-items: center;
-      justify-content: flex-end;
-      gap: 30px;
-      margin-top: 30px;
     }
 
     .experience_date {
