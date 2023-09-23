6254284ea8ab96c2083877b3c90e9774a666bdb5
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-09-23 23:08:12 +0100

    refactored code

--- src/views/CreateResume.vue
+++ src/views/CreateResume.vue
@@ -1,37 +1,9 @@
 <script setup lang="ts">
 import PageCenter from "@/components/PageCenter.vue";
-import ButtonLabel from "@/components/base-button/ButtonLabel.vue";
-import FieldLabel from "@/components/base-input/FieldLabel.vue";
-import InputCheckbox from "@/components/base-input/InputCheckbox.vue";
-import InputDate from "@/components/base-input/InputDate.vue";
-import InputMultiText from "@/components/base-input/InputMultiText.vue";
-import InputText from "@/components/base-input/InputText.vue";
-import InputTextArea from "@/components/base-input/InputTextArea.vue";
+import EducationSection from "@/components/forms/resume/EducationSection.vue";
+import ExperienceSection from "@/components/forms/resume/ExperienceSection.vue";
 import PersonalInfoSection from "@/components/forms/resume/PersonalInfoSection.vue";
-import type { EducationType } from "@/types/EducationType";
-import type { ExperienceType } from "@/types/ExperienceType";
-import { ref } from "vue";
-
-type Skills = string[];
-
-const skills = ref<Skills>(["Vue", "React", "TypeScript", "Nodejs"]);
-
-const experience = ref<ExperienceType>({
-  company: "Worklio",
-  position: "Frontend Engineer",
-  fromDate: "08/03/2023",
-  toDate: "02/13/2024", //undefined,
-});
-
-const currentJob = ref(false);
-
-const education = ref<EducationType>({
-  school: "University of Port-Harcourt",
-  degree: "Bsc", // TODO: make a dropdown for this
-  course: "Computer Science",
-  fromDate: "08/03/2023",
-  toDate: "02/13/2024", //undefined,
-});
+import SkillsSection from "@/components/forms/resume/SkillsSection.vue";
 </script>
 
 <template>
@@ -47,104 +19,9 @@ const education = ref<EducationType>({
       <main :class="$style.details">
         <div :class="$style.section">
           <PersonalInfoSection />
-
-          <div :class="$style.section">
-            <h3 :class="$style.sub_header">Skills</h3>
-            <p :class="$style.description">
-              Add your skills, separate them using commas and hit the
-              <span
-                :style="{
-                  backgroundColor: '#f0f0f0',
-                  padding: '5px 10px',
-                  borderRadius: '5px',
-                  margin: '0 5px',
-                }"
-                >Enter</span
-              >
-              key to add.
-            </p>
-            <InputMultiText v-model:data="skills" />
-          </div>
-
-          <div :class="$style.section">
-            <h3 :class="$style.sub_header">Experience</h3>
-            <p :class="$style.description">
-              Include relevant details such as job titles, company names, dates,
-              and concise descriptions of your experiences. Carefully include
-              the tasks you worked on, the tools or technologies you used and
-              how it impacted the organization.
-            </p>
-            <FieldLabel label="Company" required />
-            <InputText v-model="experience.company" required />
-            <FieldLabel label="Position" required />
-            <InputText v-model="experience.position" required />
-            <div :class="$style.experience_date">
-              <div :class="$style.dateFrom">
-                <FieldLabel label="From" required />
-                <InputDate v-model="experience.fromDate" required />
-              </div>
-              <div :class="$style.dateTo">
-                <FieldLabel label="To" required />
-                <InputDate
-                  v-model="experience.toDate"
-                  :disabled="experience.toDate === undefined"
-                  required
-                />
-              </div>
-            </div>
-            <div :class="$style.checkbox_wrap">
-              <InputCheckbox id="currentJob" v-model="currentJob" filled />
-              <label :class="$style.checkbox_label" for="currentJob">
-                I currently work here
-              </label>
-            </div>
-            <FieldLabel label="List a task you worked on" required />
-            <InputTextArea
-              placeholder="In one sentence, state a task or an action you took at the company that benefited them"
-            />
-            <FieldLabel label="Skill(s), Tools or Technologies Used" required />
-            <InputText
-              placeholder="What skills, tools or technologies did you use for the above task?"
-              required
-            />
-            <FieldLabel label="How would you rate the impact" required />
-            <div :class="$style.impact_wrap">
-              <InputText
-                placeholder="Rate the impact on a scale of 1-10"
-                type="number"
-                min="1"
-                max="10"
-                required
-              />
-              <div :class="$style.task_action">
-                <ButtonLabel label="Remove Tasks" minus />
-                <ButtonLabel label="Add New Task" plus />
-              </div>
-            </div>
-            <ButtonLabel label="Add New Experience" plus />
-          </div>
-
-          <div :class="$style.section">
-            <h3 :class="$style.sub_header">Education</h3>
-            <p :class="$style.description">
-              Provide details of your educational background, including degrees,
-              certifications, and relevant coursework.
-            </p>
-            <FieldLabel label="School" />
-            <InputText v-model="education.school" />
-            <FieldLabel label="Degree" />
-            <InputText v-model="education.degree" />
-            <div :class="$style.experience_date">
-              <div :class="$style.dateFrom">
-                <FieldLabel label="From" />
-                <InputDate v-model="education.fromDate" />
-              </div>
-              <div :class="$style.dateTo">
-                <FieldLabel label="To" />
-                <InputDate v-model="education.toDate" />
-              </div>
-            </div>
-          </div>
+          <ExperienceSection />
+          <SkillsSection />
+          <EducationSection />
         </div>
       </main>
     </div>
@@ -183,30 +60,6 @@ const education = ref<EducationType>({
 
       .dateFrom,
       .dateTo {
-        width: 100%;
-      }
-    }
-
-    .checkbox_wrap {
-      display: flex;
-      align-items: center;
-      gap: 15px;
-      margin-top: 30px;
-
-      .checkbox_label {
-        cursor: pointer;
-        user-select: none;
-      }
-    }
-
-    .impact_wrap {
-      display: flex;
-      gap: 20px;
-      margin-bottom: 30px;
-
-      .task_action {
-        display: flex;
-        gap: 20px;
         width: 100%;
       }
     }
