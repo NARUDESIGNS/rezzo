de9bb292f5b99bd97cc0a03c6ef28b98f5fec97f
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-09-30 23:37:23 +0100

    WIP: base popup in progress

--- src/components/base-popup/BasePopup.vue
+++ src/components/base-popup/BasePopup.vue
@@ -1,11 +1,28 @@
 <script setup lang="ts">
-// code here...
+import XIcon from "@/assets/XIcon.vue";
+import BaseBackdrop from "../base-backdrop/BaseBackdrop.vue";
+
+// defineProps<{
+//   modelValue: boolean;
+// }>();
 </script>
 
 <template>
-  <!-- component here... -->
+  <BaseBackdrop />
+  <div :class="$style.wrap">
+    <XIcon />
+    <slot />
+  </div>
 </template>
 
 <style module lang="scss">
-// styles here...
+@use "@/scss/colors";
+
+.wrap {
+  border-radius: 10px;
+  background-color: colors.use("background-light");
+  width: 300px;
+  height: 300px;
+  padding: 20px;
+}
 </style>
