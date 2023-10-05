acf8d8e9f0b32f48e8a6c1f008f27bbbf21814c1
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-10-05 22:37:19 +0100

    added template for helper form

---
+++ src/components/forms/new-fields/AddPersonalInfo.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+// script here...
+</script>
+
+<template>
+  <div>
+    <!-- template here... -->
+  </div>
+</template>
+
+<style scoped>
+/* styles here... */
+</style>
