c49db793b359d9405c7712a2c95b790fd2771205
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-10-09 21:34:34 +0100

    added modal form to add new personal info

--- src/components/forms/new-fields/AddPersonalInfo.vue
+++ src/components/forms/new-fields/AddPersonalInfo.vue
@@ -1,13 +1,49 @@
 <script setup lang="ts">
-// script here...
+import BaseButton from "@/components/base-button/BaseButton.vue";
+import BaseDialog from "@/components/base-dialog/BaseDialog.vue";
+import FieldLabel from "@/components/base-input/FieldLabel.vue";
+import InputText from "@/components/base-input/InputText.vue";
+import { computed, ref } from "vue";
+
+const props = defineProps<{
+  visible: boolean;
+}>();
+
+const emit = defineEmits<{
+  (e: "close"): void;
+}>();
+
+const showModal = computed({
+  get: () => props.visible,
+  set: () => emit("close"),
+});
+
+const title = ref("Github");
+const value = ref("https://github.com/narudesigns");
 </script>
 
 <template>
   <div>
-    <!-- template here... -->
+    <BaseDialog v-model="showModal" disable-close>
+      <template #header>
+        <h3>Add New Field</h3>
+      </template>
+      <div :class="$style.form">
+        <FieldLabel label="Title" required />
+        <InputText v-model="title" required />
+        <FieldLabel label="Value" required />
+        <InputText v-model="value" required />
+      </div>
+      <template #buttons>
+        <BaseButton danger @click="$emit('close')"> Cancel </BaseButton>
+        <BaseButton> Add Field </BaseButton>
+      </template>
+    </BaseDialog>
   </div>
 </template>
 
-<style scoped>
-/* styles here... */
+<style lang="scss" module>
+.form :first-child {
+  margin-top: 0;
+}
 </style>
