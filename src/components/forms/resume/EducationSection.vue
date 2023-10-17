943427d2b3cb8199099d52e02803d6f10c556047
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-10-17 22:43:14 +0100

    WIP: type error | restructuring in progress...

--- src/components/forms/resume/EducationSection.vue
+++ src/components/forms/resume/EducationSection.vue
@@ -3,16 +3,32 @@ import ButtonLabel from "@/components/base-button/ButtonLabel.vue";
 import FieldLabel from "@/components/base-input/FieldLabel.vue";
 import InputDate from "@/components/base-input/InputDate.vue";
 import InputText from "@/components/base-input/InputText.vue";
-import type { EducationType } from "@/types/EducationType";
+import { NewEducationType } from "@/types/EducationType";
+import type { FieldType } from "@/types/FieldType";
 import { ref } from "vue";
+import AddEducation from "../new-fields/AddEducation.vue";
 
-const education = ref<EducationType>({
-  school: "University of Port-Harcourt",
-  degree: "Bsc", // TODO: make a dropdown for this
-  course: "Computer Science",
-  fromDate: "08/03/2023",
-  toDate: "02/13/2024", //undefined,
-});
+const educationData = ref<FieldType[][]>([
+  [
+    { title: "School", value: "University of Port-Harcourt", required: true },
+    { title: "Degree", value: "Bsc Computer Science", required: true }, // TODO: make a dropdown for this
+    { title: "From", value: "08/03/2023", required: true },
+    { title: "To", value: "02/13/2024", required: true },
+  ],
+  [
+    { title: "School", value: "University of Benin", required: false },
+    { title: "Degree", value: "Law", required: false }, // TODO: make a dropdown for this
+    { title: "From", value: "04/06/2023", required: true },
+    { title: "To", value: "02/23/2025", required: true },
+  ],
+]);
+
+const openNewEducationModal = ref(false);
+
+const addNewField = (data: NewEducationType) => {
+  // educationData.value.push()
+  console.log(data);
+};
 </script>
 
 <template>
@@ -22,24 +38,43 @@ const education = ref<EducationType>({
       Provide details of your educational background, including degrees,
       certifications, and relevant coursework.
     </p>
-    <FieldLabel label="School" />
-    <InputText v-model="education.school" />
-    <FieldLabel label="Degree" />
-    <InputText v-model="education.degree" />
-    <div :class="$style.date_wrap">
-      <div :class="$style.dateFrom">
-        <FieldLabel label="From" />
-        <InputDate v-model="education.fromDate" />
-      </div>
-      <div :class="$style.dateTo">
-        <FieldLabel label="To" />
-        <InputDate v-model="education.toDate" />
+    <div
+      v-for="(education, index) in educationData"
+      :key="index"
+      :class="$style.education"
+    >
+      <div v-for="(data, _index) in education" :key="_index">
+        <template v-if="data.title !== 'From' && data.title !== 'To'">
+          <FieldLabel :label="data.title" :required="data.required" />
+          <InputText v-model="data.value" :required="data.required" />
+        </template>
+        <div :class="$style.date_wrap">
+          <div v-if="data.title === 'From'" :class="$style.dateFrom">
+            <FieldLabel :label="data.title" :required="data.required" />
+            <InputDate v-model="data.value" :required="data.required" />
+          </div>
+          <div v-else-if="data.title === 'To'" :class="$style.dateTo">
+            <FieldLabel :label="data.title" :required="data.required" />
+            <InputDate v-model="data.value" :required="data.required" />
+          </div>
+        </div>
       </div>
     </div>
+
     <div :class="$style.btn_label_wrap">
-      <ButtonLabel label="Add Education" plus />
+      <ButtonLabel
+        label="Add Education"
+        @click="openNewEducationModal = true"
+        plus
+      />
       <ButtonLabel label="Remove Education" minus />
     </div>
+
+    <AddEducation
+      :visible="openNewEducationModal"
+      @add-field="(data) => addNewField(data)"
+      @close="openNewEducationModal = false"
+    />
   </div>
 </template>
 
@@ -52,6 +87,10 @@ const education = ref<EducationType>({
   .sub_header {
     color: colors.use("primary");
     margin: 50px 0 10px 0;
+  }
+
+  .education:not(:last-of-type) {
+    margin-bottom: 50px;
   }
 
   .date_wrap {
