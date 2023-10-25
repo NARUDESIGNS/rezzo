34c752fa872ab1f97e9c7e9e648515785a3066a0
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-10-26 00:10:13 +0100

    simplified type for education data

--- src/types/EducationType.ts
+++ src/types/EducationType.ts
@@ -2,13 +2,6 @@ export type EducationType = {
   school: string;
   degree: string;
   course?: string;
-  fromDate: string;
-  toDate: string;
-};
-
-export type NewEducationType = {
-  school: string;
-  degree: string;
   startDate: string;
   endDate: string;
 };
