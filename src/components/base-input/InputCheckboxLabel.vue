dee6524c40affa0ceaf243f285d3b6ebb23125e7
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-10-30 23:49:01 +0100

    added new checkbox with label component

---
+++ src/components/base-input/InputCheckboxLabel.vue
@@ -0,0 +1,32 @@
+<script setup lang="ts">
+import InputCheckbox from "./InputCheckbox.vue";
+defineProps<{
+  modelValue?: boolean;
+  label?: string;
+}>();
+
+const uniqueID = Date.now().toString();
+</script>
+
+<template>
+  <div :class="$style.checkbox_wrap">
+    <InputCheckbox :id="uniqueID" :checked="modelValue" />
+    <label :class="$style.checkbox_label" :for="uniqueID">
+      <slot> {{ label }} </slot>
+    </label>
+  </div>
+</template>
+
+<style module lang="scss">
+.checkbox_wrap {
+  display: flex;
+  align-items: center;
+  gap: 15px;
+  margin: 30px 0;
+
+  .checkbox_label {
+    cursor: pointer;
+    user-select: none;
+  }
+}
+</style>
