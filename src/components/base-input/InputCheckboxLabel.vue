a14c319a685600dfc5980951d6e78ebea8dccbb5
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2023-11-16 23:48:24 +0100

    WIP: remove education feature

--- src/components/base-input/InputCheckboxLabel.vue
+++ src/components/base-input/InputCheckboxLabel.vue
@@ -1,18 +1,30 @@
 <script setup lang="ts">
+import { ref } from "vue";
 import InputCheckbox from "./InputCheckbox.vue";
+
 defineProps<{
   modelValue?: boolean;
   label?: string;
 }>();
 
-const uniqueID = Date.now().toString();
+const emit = defineEmits<{
+  (e: "update:modelValue", value: boolean): void;
+}>();
+
+const value = ref();
+const generateID = () => new Date().getTime().toString() + Math.random();
+const uniqueID = ref(generateID());
 </script>
 
 <template>
   <div :class="$style.checkbox_wrap">
-    <InputCheckbox :id="uniqueID" :checked="modelValue" />
+    <InputCheckbox
+      :id="uniqueID"
+      v-model="value"
+      @update:model-vaue="(value) => $emit('update:modelValue', value)"
+    />
     <label :class="$style.checkbox_label" :for="uniqueID">
-      <slot> {{ label }} </slot>
+      <slot> {{ label }} </slot> {{ modelValue }}
     </label>
   </div>
 </template>
