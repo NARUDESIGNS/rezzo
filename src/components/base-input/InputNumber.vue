edc930b4f0529e28ef3e284f475e680588433746
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2024-06-20 08:45:30 +0100

    WIP: updated number input component

--- src/components/base-input/InputNumber.vue
+++ src/components/base-input/InputNumber.vue
@@ -1,15 +1,42 @@
 <script setup lang="ts">
+import { useVModel } from "@vueuse/core";
+import { watch } from "vue";
 import InputText from "./InputText.vue";
-defineProps<{
-  /** Minimum number */
-  min?: number;
-  /** Maximum number */
-  max?: number;
+
+const props = withDefaults(
+  defineProps<{
+    /** Model value */
+    modelValue?: number;
+    /** Minimum number */
+    min?: number;
+    /** Maximum number */
+    max?: number;
+  }>(),
+  {
+    modelValue: undefined,
+    min: 0,
+    max: 10,
+  }
+);
+
+const emit = defineEmits<{
+  (e: "update:modelValue", value: number | string): void;
 }>();
+
+const num = useVModel(props, "modelValue", emit);
+
+watch(num, () => {
+  if (num.value && num.value > props.max) num.value = props.max;
+  else if (num.value && num.value < props.min) num.value = props.min;
+  else num.value = Number(num.value);
+});
+
+// TODO: Fix v-model type
+// sterilize input
 </script>
 
 <template>
-  <InputText type="number" :min="min" :max="max" />
+  <InputText v-model="num" type="number" :min="min" :max="max" />
 </template>
 
 <style module lang="scss">
