601eda3632e1ad20966b8c48e016cf9140d25614
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2025-01-02 00:10:45 +0100

    WIP: sample data for resume preview

--- src/views/PreviewResumeView.vue
+++ src/views/PreviewResumeView.vue
@@ -4,7 +4,112 @@ import { useResumeDataStore } from "@/store/useResumeDataStore";
 import { storeToRefs } from "pinia";
 
 const resumeStore = useResumeDataStore();
-const { resumeData } = storeToRefs(resumeStore);
+const { _resumeData } = storeToRefs(resumeStore);
+
+const resumeData = {
+  personalInfo: [
+    {
+      title: "Full Name",
+      value: "Paul Ibeabuchi",
+      required: true,
+    },
+    {
+      title: "Email",
+      value: "ibeabuchipaul@gmail.com",
+      required: true,
+    },
+    {
+      title: "Phone",
+      value: "08160564736",
+      required: false,
+    },
+    {
+      title: "Location",
+      value: "Lagos, Nigeria",
+      required: true,
+    },
+    {
+      title: "LinkedIn",
+      value: "https://linkedin.com/in/narudesigns",
+      required: false,
+    },
+  ],
+  experience: [
+    {
+      company: "OPay",
+      position: "Technical Support Engineer",
+      isCurrentPosition: false,
+      endDate: "01/08/2024",
+      startDate: "31/08/2024",
+      tasks: [
+        {
+          summary:
+            "Created demo and workshop materials for production merchants",
+          skills: ["Microsft Word", "Power Point", "Microsoft Teams"],
+          impact: "This was used by over 300 merchants",
+        },
+        {
+          summary: "Provided integration and production support to merchants",
+          skills: ["Microsoft Teams", "Communication", "APIs", "Documentation"],
+          impact: "This increased revenue by 25%",
+        },
+      ],
+    },
+    {
+      company: "Worklio",
+      position: "Senior Frontend Engineer",
+      isCurrentPosition: true,
+      endDate: "24/05/2022",
+      startDate: "31/07/2024",
+      tasks: [
+        {
+          summary: "Built reusable components for UI application",
+          skills: [
+            "Vuejs",
+            "TypeScript",
+            "Git",
+            "SCSS",
+            "CSS3",
+            "JavaScript",
+            "VsCode IDE",
+          ],
+          impact: "20",
+        },
+      ],
+    },
+  ],
+  skills: [
+    "MongoDB",
+    "React",
+    "Vuejs",
+    "Nodejs",
+    "Git",
+    "SCSS",
+    "CSS3",
+    "HTML5",
+    "MySQL",
+    "Bootstrap",
+    "Firebase",
+    "Jest",
+    "Vitest",
+  ],
+  education: [
+    {
+      school: "University of Port Harcourt",
+      course: "Computer Science",
+      degree: "Bachelor of Science (BS)",
+      startDate: "01/07/2024",
+      endDate: "31/08/2024",
+    },
+    {
+      school: "University of Florida",
+      course: "AI and Machine Learning",
+      degree: "Bachelor of Science (BS)",
+      startDate: "31/07/2024",
+      endDate: "20/08/2024",
+    },
+  ],
+};
 
 function generateAchievmentPoint(
   summary: string,
