d47c845caf882ead6688c77c82c793a2b7eaf05f
Paul Ibeabuchi <ibeabuchipaul@gmail.com>
2025-01-06 23:44:34 +0100

    added tsdoc comments

--- src/types/EducationType.ts
+++ src/types/EducationType.ts
@@ -1,7 +1,12 @@
 export type EducationType = {
+  /** School */
   school: string;
+  /** Degree */
   degree: string;
+  /** Course */
   course?: string;
+  /** Start Date */
   startDate: string;
+  /** End date */
   endDate: string;
 };
--- src/types/ExperienceType.ts
+++ src/types/ExperienceType.ts
@@ -1,12 +1,21 @@
 export interface ExperienceType {
+  /** Company */
   company: string;
+  /** Job position */
   position: string;
-  isCurrentPosition: boolean;
+  /** Employee still works at this company  */
+  isCurrentCompany: boolean;
+  /** Start date */
   startDate: string;
+  /** End Date */
   endDate: string;
+  /** Achievements */
   tasks: Array<{
+    /** Achievement summary */
     summary: string;
+    /** Skills associated with achievement */
     skills: string[];
+    /** Impact of achievement */
     impact?: string;
   }>;
 }
